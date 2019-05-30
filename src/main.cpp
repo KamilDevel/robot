8225e0b9e784aacc5d142a45047e96d7ccb57f63
Kamil Devel <kamil.devel@gmail.com>
2019-05-30 21:05:16 +0200

    Added basic cmake config and main program

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,11 @@
 #include "./main.hpp"
+#include <iostream>
+
+
+using namespace std;
 
 int main(int argc, char** argv) {
+    cout << "Hellow World EV3!" << endl;
 
     return 0;
 }
--- src/main.hpp
+++ src/main.hpp
@@ -1,4 +1,15 @@
 #ifndef ROBOT_MAIN_HPP
 #define ROBOT_MAIN_HPP
 
+/* Version definition */
+#define Robot_VERSION_MAJOR 0
+#define Robot_VERSION_MINOR 0
+#define Robot_VERSION_PATCH 1
+
+/* Number of bricks connected */
+#define Robot_NUMBER_OF_BRICKS 1
+
+/* Build code name */
+#define Robot_CODENAME APLHA
+
 #endif
