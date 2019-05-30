2e6e39b8630fa93337de5058d21a0a2d76f748df
Kamil Devel <kamil.devel@gmail.com>
2019-05-30 23:11:25 +0200

    First Application entry point and minor fixes

---
+++ src/Robot/Application.cpp
@@ -0,0 +1,18 @@
+#include "./Application.hpp"
+
+/**
+ * Default constructor.
+ */
+
+Robot::Application::Application()
+{
+
+}
+
+/**
+ * Default destructor.
+ */
+Robot::Application::~Application()
+{
+
+}
---
+++ src/Robot/Application.hpp
@@ -0,0 +1,12 @@
+#ifndef ROBOT_APPLICATION_HPP
+#define ROBOT_APPLICATION_HPP
+
+namespace Robot {
+    class Application {
+        public:
+            Application();
+            ~Application();
+    };
+};
+
+#endif
